395db9e6c5e0e4eb65d673b8f918134d113954ff
liviaAugusta <liviamattoni@gmail.com>
2022-05-03 18:37:55 +0200

    aggiunta commenti e viasualiz console

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
+//chiedo all'utente come si chiama, qual è il suo cognome e quae sia il suo colore preferito//
+//dai dati ottenuti dall'utente genero una password//
+
 const nome = prompt("Come ti chiami?");
+
+console.log(nome);
 
 const cognome = prompt("Scrivi il tuo cognome");
 
+console.log(cognome);
+
 const colore = prompt("qual è il tuo colore preferito?");
 
+console.log(colore);
+
 document.getElementById("mio_id").innerHTML = `${nome}${cognome}${colore}21` ; 
+
