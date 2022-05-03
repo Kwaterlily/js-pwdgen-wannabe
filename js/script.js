346941b8e26fbad9039234df6e988f49fb1ce97a
liviaAugusta <liviamattoni@gmail.com>
2022-05-03 18:21:28 +0200

    js-pwdgen-wannabe

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const nome = prompt("Come ti chiami?");
+
+const cognome = prompt("Scrivi il tuo cognome");
+
+const colore = prompt("qual è il tuo colore preferito?");
+
+document.getElementById("mio_id").innerHTML = `${nome}${cognome}${colore}21` ; 
